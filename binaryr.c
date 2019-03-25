397bbb9d7a318b704125718eef85264a8236491a
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-03-25 23:46:45 +0530

    binaryr.c

---
+++ binaryr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n,r,b1=1,b=0,c=0;
+   scanf("%d",&n);
+   while(n)
+   {
+       
+       r=n%2;
+       if(r==1)
+       {
+         c++;
+       }
+       n=n/2;
+       b=b+(r*b1);
+      b1=b1*10;
+      
+   }
+   printf("%d",c);
+
+    return 0;
+}
+
