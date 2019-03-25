d67a58e6507d9dd221668196b89599c5d3fffacb
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-03-25 23:15:13 +0530

    pair.c

---
+++ pair.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int n,m,i,j,a,b,s=0;
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&a,&b);
+        if(b==m)
+        {
+            s++;
+        }
+    }
+    if(s>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}
+
